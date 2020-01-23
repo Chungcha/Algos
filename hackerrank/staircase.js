2fe6cb68b61c455b002b332103ef5c7aa8f1e220
chungcha <charlieschung@gmail.com>
2020-01-23 15:11:01 -0500

    staircase

--- hackerrank/staircase.js
+++ hackerrank/staircase.js
@@ -7,3 +7,14 @@ function staircase(n) {
         console.log(space.repeat(n-i)+hash.repeat(i))
     }
 }
+
+// even more clean 
+
+function staircase(n) {
+    for (let i = 1; i <= n; i++){
+        console.log("#".repeat(i).padStart(n))
+    }
+
+    // so "#" repeats the string i amount of times. padStart appends a string of a certain length (n) in front of the current string.  If it already is the length, nothing is added. First input is the target length and second can be the ouput string
+
+}
